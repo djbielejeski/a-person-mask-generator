1153a93cfcadc50226636af87ead6f4a81f11d93
David Bielejeski <2463282+djbielejeski@users.noreply.github.com>
2024-05-27 08:34:31 -0500

    Fixing install for auto1111

--- install.py
+++ install.py
@@ -1,29 +1,30 @@
-import sys
 import os
-import subprocess
+
 
 def install_package(package_name):
-    subprocess.check_call([sys.executable, "-m", "pip", "install", package_name])
+    try:
+        # Auto1111 install
+        __import__('launch')
+        import launch
+        if not launch.is_installed(package_name):
+            launch.run_pip(f"install {package_name}", f"a-person-mask-generator requirement: {package_name}")
+    except ImportError:
+        # ComfyUI install
+        import sys
+        import subprocess
+        subprocess.check_call([sys.executable, "-m", "pip", "install", package_name])
 
 
-req_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), "requirements.txt")
+def install_dependencies():
+    req_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), "requirements.txt")
 
-with open(req_file) as file:
-    for package in file:
-        package = package.strip()
-        try:
-            __import__(package)
-        except ImportError:
-            install_package(package)
+    with open(req_file) as file:
+        for package in file:
+            package = package.strip()
+            try:
+                __import__(package)
+            except ImportError:
+                install_package(package)
 
-from .a_person_mask_generator_comfyui import APersonMaskGenerator
 
-NODE_CLASS_MAPPINGS = {
-    "APersonMaskGenerator": APersonMaskGenerator,
-}
-
-NODE_DISPLAY_NAME_MAPPINGS = {
-    "APersonMaskGenerator": "A Person Mask Generator"
-}
-
-__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
+install_dependencies()
